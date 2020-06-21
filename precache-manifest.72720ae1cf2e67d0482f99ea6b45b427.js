84158f366c5ce0334c62a3fd8be59a7515f44bbf
Venkata Ramana <venkata.ramana@ad3ihyd.local>
2020-06-21 13:13:27 +0530

    Updates

---
+++ precache-manifest.72720ae1cf2e67d0482f99ea6b45b427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4314c648bf47256135bc8ba004d28d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cb263aeaab756b8e8c06",
+    "url": "/monsters-rolodex/static/css/main.e20d44a2.chunk.css"
+  },
+  {
+    "revision": "e1ac61fa466c2396d439",
+    "url": "/monsters-rolodex/static/js/2.fe564023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.fe564023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb263aeaab756b8e8c06",
+    "url": "/monsters-rolodex/static/js/main.f33a989f.chunk.js"
+  },
+  {
+    "revision": "40be79e4566793ee2bc9",
+    "url": "/monsters-rolodex/static/js/runtime-main.cb85336a.js"
+  }
+]);
